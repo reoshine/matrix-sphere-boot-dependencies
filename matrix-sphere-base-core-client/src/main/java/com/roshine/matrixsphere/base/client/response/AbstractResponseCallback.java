a5073e1de8c5268e514e030b3bc5bac370370e79
roshine <reoshine@163.com>
2024-12-02 10:47:32 +0800

    fix: 修改Jenkinsfile

--- matrix-sphere-base-core-client/src/main/java/com/roshine/matrixsphere/base/client/response/AbstractResponseCallback.java
+++ matrix-sphere-base-core-client/src/main/java/com/roshine/matrixsphere/base/client/response/AbstractResponseCallback.java
@@ -4,21 +4,21 @@ package com.roshine.matrixsphere.base.client.response;
  * @author roshine
  * @version 1.0.0
  * @date 2020-10-26 23:45
- * @Description 定义抽象结果处理
+ * 定义抽象结果处理
  */
 public abstract class AbstractResponseCallback<T> {
 
     /**
      * 成功结果处理
-     * @param response
-     * @return
+     * @param response 响应体
+     * @return Response<T>
      */
     public abstract Response<T> success(Response<T> response);
 
     /**
      * 失败结果处理
-     * @param response
-     * @return
+     * @param response 响应体
+     * @return Response<T>
      */
     public Response<T> fail(Response<T> response) {
         return response;
